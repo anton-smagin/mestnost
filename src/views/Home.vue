a46f658f94df7ad45269ae45bd1194a665ef111e
Anton Smagin <a.smagin@gettransfer.com>
2021-02-12 19:20:29 +0300

    init

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <p-5-sketch />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import P5Sketch from '@/components/P5Sketch.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    P5Sketch
+  }
+}
+</script>
