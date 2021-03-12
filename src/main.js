3c64da83f8da8c76845f208da82f7d006fe1be83
Anton Smagin <a.smagin@gettransfer.com>
2021-03-12 17:41:25 +0300

    Yandex-metrika builld

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueYandexMetrika from 'vue-yandex-metrika'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faApple, faYandex, faBandcamp, faSpotify } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
@@ -12,6 +13,14 @@ library.add(faSpotify)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
+
+Vue.use(VueYandexMetrika, {
+  id: 73630327,
+  router: router,
+  env: process.env.NODE_ENV,
+  debug: true,
+  // other options
+})
 
 new Vue({
   router,
